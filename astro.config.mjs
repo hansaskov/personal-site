04c594892d47c2be3c0782378a907dcb3e7cf0f2
hansaskov <hansaskov1999@gmail.com>
2024-01-11 13:32:33 +0100

    Change runtime to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,12 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://hans.askov.dk',
   integrations: [mdx(), sitemap(), tailwind()],
-  output: "hybrid",
+  output: "static",
   adapter: vercel(),
 });
