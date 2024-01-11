acb947d491ebbaa097c64e7174d4e7a48b47a648
Hans Askov <70580312+hansaskov@users.noreply.github.com>
2024-01-11 13:13:37 +0100

    Initial commit

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Hans Askov.dk | Portfolio & CV';
+export const SITE_DESCRIPTION = 'This is a personal portfolio for Hans Askov to showcase his projects and introduce him self with his CV.';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
