be152d4eb29f0685deee193d5f921a9d848e329d
Hans Askov <hans@askov.dk>
2026-06-11 14:02:51 +0200

    Update name of cv f

--- playwright/pdf.spec.ts
+++ playwright/pdf.spec.ts
@@ -7,7 +7,7 @@ if (!baseURL) throw new Error('BASE_URL environment variable is not set');
 test('get started link', async ({ page }) => {
   await page.goto(`${baseURL}/cv`);
   await page.pdf({
-    path: './playwright/cv.pdf',
+    path: './playwright/hans-askov-cv.pdf',
     printBackground: true,
     tagged: true,
     height: 2048,
