3090ed71bbb519ba6c484a4dfdfbbfed9eff9936
Hans Askov <hans@askov.dk>
2026-06-11 21:45:38 +0200

    Small code changes

--- src/config.ts
+++
@@ -1,6 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "Hans Askov.dk | Portfolio & CV";
-export const SITE_DESCRIPTION =
-  "This is a personal portfolio for Hans Askov to showcase his projects and introduce him self with his CV.";
