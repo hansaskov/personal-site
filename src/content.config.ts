9e832d5493fd28c43ce4aa2d1a2cebf2726f88d8
Hans Askov <hansaskov1999@gmail.com>
2026-08-20 22:50:36 +0200

    Update to use collections for cv's

--- src/content.config.ts
+++ src/content.config.ts
@@ -8,4 +8,34 @@ const applicationLetters = defineCollection({
   }),
 });
 
-export const collections = { applicationLetters };
+const cvs = defineCollection({
+  loader: glob({ pattern: "**/*.json", base: "./src/content/cvs" }),
+  schema: ({ image }) => {
+    const role = z.object({
+      title: z.string(),
+      year: z.string(),
+      duration: z.string(),
+      location: z.string(),
+      description: z.string(),
+    });
+
+    const timeline = z.object({
+      company: z.string(),
+      year: z.string(),
+      duration: z.string(),
+      img: image(),
+      roles: z.array(role),
+    });
+
+    return z.object({
+      slug: z.string(),
+      title: z.string(),
+      intro: z.string(),
+      skills: z.array(z.string()),
+      experiances: z.array(timeline),
+      educations: z.array(timeline),
+    });
+  },
+});
+
+export const collections = { applicationLetters, cvs };
